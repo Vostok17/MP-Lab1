8c787901509ff0426917a4bd647e5f001899d7d5
Vostok17 <gagarin.artem.2008@gmail.com>
2022-02-08 00:28:03 +0300

    Change tabs to spaces

--- task1.cpp
+++ task1.cpp
@@ -4,131 +4,131 @@ using namespace std;
 
 int main()
 {
-	const int DisplayedWordsCtn = 25;
+    const int DisplayedWordsCtn = 25;
 
-	const int StopWordsCtn = 12;
-	string stopWords[] = { "at", "for", "the", "in", "before", "on", "so", "a", "than", "to", "with", "by" };
+    const int StopWordsCtn = 12;
+    string stopWords[] = { "at", "for", "the", "in", "before", "on", "so", "a", "than", "to", "with", "by" };
 
-	const int MaxWords = 10000;
-	int* wordRepeats = new int[MaxWords] {};
-	string* words = new string[MaxWords];
+    const int MaxWords = 10000;
+    int* wordRepeats = new int[MaxWords] {};
+    string* words = new string[MaxWords];
 
-	string word;
+    string word;
 
-	ifstream fin;
-	fin.open("input.txt");
+    ifstream fin;
+    fin.open("input.txt");
 
-	int wordsCtn = 0;
+    int wordsCtn = 0;
 loop_input:
-	if (fin >> word)
-	{
-		bool isNewWord = true;
+    if (fin >> word)
+    {
+        bool isNewWord = true;
 
-		int i = 0;
-		string fixedWord = "";
-	process_word:
-		if (word[i] != '\0')
-		{
-			char c = word[i];
-			if ((c >= 'a' && c <= 'z') ||
-				(c >= 'A' && c <= 'Z') ||
-				(c == '-')) // allowed chars
-			{
-				if (c >= 'A' && c <= 'Z')
-				{
-					c += 'a' - 'A';
-				}
-				fixedWord += c;
-			}
-			i++;
-			goto process_word;
-		}	
-		word = fixedWord;
+        int i = 0;
+        string fixedWord = "";
+    process_word:
+        if (word[i] != '\0')
+        {
+            char c = word[i];
+            if ((c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c == '-')) // allowed chars
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    c += 'a' - 'A';
+                }
+                fixedWord += c;
+            }
+            i++;
+            goto process_word;
+        }	
+        word = fixedWord;
 
-		if (word == "")
-		{
-			goto loop_input;
-		}
+        if (word == "")
+        {
+            goto loop_input;
+        }
 
-		i = 0;
-	check_stop_words:
-		if (i < StopWordsCtn)
-		{
-			if (stopWords[i] == word)
-			{
-				goto loop_input;
-			}
-			i++;
-			goto check_stop_words;
-		}
+        i = 0;
+    check_stop_words:
+        if (i < StopWordsCtn)
+        {
+            if (stopWords[i] == word)
+            {
+                goto loop_input;
+            }
+            i++;
+            goto check_stop_words;
+        }
 
-		i = 0;
-	check_word_repeats:
-		if (i < wordsCtn)
-		{
-			if (words[i] == word)
-			{
-				wordRepeats[i]++;
-				isNewWord = false;
-			}
+        i = 0;
+    check_word_repeats:
+        if (i < wordsCtn)
+        {
+            if (words[i] == word)
+            {
+                wordRepeats[i]++;
+                isNewWord = false;
+            }
 
-			i++;
-			goto check_word_repeats;
-		}
+            i++;
+            goto check_word_repeats;
+        }
 
-		if (isNewWord)
-		{
-			words[wordsCtn] = word;
-			wordRepeats[wordsCtn] = 1;
-			wordsCtn++;
-		}
-		goto loop_input;
-	}
-	fin.close();
+        if (isNewWord)
+        {
+            words[wordsCtn] = word;
+            wordRepeats[wordsCtn] = 1;
+            wordsCtn++;
+        }
+        goto loop_input;
+    }
+    fin.close();
 
-	// Insertion sort
-	int i = 1;
+    // Insertion sort
+    int i = 1;
 loop_i:
-	if (i < wordsCtn)
-	{
-		int j = i;
-	loop_j:
-		if (j > 0 && wordRepeats[j - 1] < wordRepeats[j])
-		{
-			int temp = wordRepeats[j];
-			wordRepeats[j] = wordRepeats[j - 1];
-			wordRepeats[j - 1] = temp;
+    if (i < wordsCtn)
+    {
+        int j = i;
+    loop_j:
+        if (j > 0 && wordRepeats[j - 1] < wordRepeats[j])
+        {
+            int temp = wordRepeats[j];
+            wordRepeats[j] = wordRepeats[j - 1];
+            wordRepeats[j - 1] = temp;
 
-			string tempWord = words[j];
-			words[j] = words[j - 1];
-			words[j - 1] = tempWord;
+            string tempWord = words[j];
+            words[j] = words[j - 1];
+            words[j - 1] = tempWord;
 
-			j--;
-			goto loop_j;
-		}
+            j--;
+            goto loop_j;
+        }
 
-		i++;
-		goto loop_i;
-	}
+        i++;
+        goto loop_i;
+    }
 
-	ofstream fout;;
-	fout.open("output.txt");
+    ofstream fout;;
+    fout.open("output.txt");
 
-	i = 0;
+    i = 0;
 loop_output:
-	if (i < DisplayedWordsCtn)
-	{
-		if (i < wordsCtn)
-		{
-			fout << words[i] << " - " << wordRepeats[i] << endl;
-			i++;
-			goto loop_output;
-		}	
-	}
-	fout.close();
+    if (i < DisplayedWordsCtn)
+    {
+        if (i < wordsCtn)
+        {
+            fout << words[i] << " - " << wordRepeats[i] << endl;
+            i++;
+            goto loop_output;
+        }	
+    }
+    fout.close();
 
-	delete[] wordRepeats, words;
+    delete[] wordRepeats, words;
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
--- task2.cpp
+++ task2.cpp
@@ -4,155 +4,155 @@ using namespace std;
 
 int main()
 {
-	const int StopWordsCtn = 12;
-	string stopWords[] = { "at", "for", "the", "in", "before", "on", "so", "a", "than", "to", "with", "by" };
+    const int StopWordsCtn = 12;
+    string stopWords[] = { "at", "for", "the", "in", "before", "on", "so", "a", "than", "to", "with", "by" };
 
-	const int MaxWords = 10000;
-	int* wordRepeats = new int[MaxWords] {};
-	string* words = new string[MaxWords];
+    const int MaxWords = 10000;
+    int* wordRepeats = new int[MaxWords] {};
+    string* words = new string[MaxWords];
 
-	int** wordPages = new int* [MaxWords];
-	const int MaxPages = 100;
+    int** wordPages = new int* [MaxWords];
+    const int MaxPages = 100;
 
-	int i = 0;
+    int i = 0;
 init_word_pages:
-	if (i < MaxWords)
-	{
-		wordPages[i] = new int[MaxPages] {};
-		i++;
-		goto init_word_pages;
-	}
+    if (i < MaxWords)
+    {
+        wordPages[i] = new int[MaxPages] {};
+        i++;
+        goto init_word_pages;
+    }
 
-	string word;
+    string word;
 
-	ifstream fin;
-	fin.open("input.txt");
+    ifstream fin;
+    fin.open("input.txt");
 
-	int wordsCtn = 0,
-		cutWordsCtn = 0,
-		currString = 0;
+    int wordsCtn = 0,
+        cutWordsCtn = 0,
+        currString = 0;
 loop_input:
-	if (fin >> word)
-	{
-		if (fin.get() == '\n')
-		{
-			currString++;
-		}
+    if (fin >> word)
+    {
+        if (fin.get() == '\n')
+        {
+            currString++;
+        }
 
-		bool isNewWord = true;
+        bool isNewWord = true;
 
-		int i = 0;
-		string fixedWord = "";
-	process_word:
-		if (word[i] != '\0')
-		{
-			char c = word[i];
-			if ((c >= 'a' && c <= 'z') ||
-				(c >= 'A' && c <= 'Z') ||
-				(c == '-')) // allowed chars
-			{
-				if (c >= 'A' && c <= 'Z')
-				{
-					c += 'a' - 'A';
-				}
-				fixedWord += c;
-			}
-			i++;
-			goto process_word;
-		}
-		word = fixedWord;
+        int i = 0;
+        string fixedWord = "";
+    process_word:
+        if (word[i] != '\0')
+        {
+            char c = word[i];
+            if ((c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c == '-')) // allowed chars
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    c += 'a' - 'A';
+                }
+                fixedWord += c;
+            }
+            i++;
+            goto process_word;
+        }
+        word = fixedWord;
 
-		if (word == "")
-		{
-			goto loop_input;
-		}
+        if (word == "")
+        {
+            goto loop_input;
+        }
 
-		i = 0;
-	check_stop_words:
-		if (i < StopWordsCtn)
-		{
-			if (stopWords[i] == word)
-			{
-				goto loop_input;
-			}
-			i++;
-			goto check_stop_words;
-		}
+        i = 0;
+    check_stop_words:
+        if (i < StopWordsCtn)
+        {
+            if (stopWords[i] == word)
+            {
+                goto loop_input;
+            }
+            i++;
+            goto check_stop_words;
+        }
 
-		i = 0;
-	check_word_repeats:
-		if (i < wordsCtn)
-		{
-			if (words[i] == word)
-			{
-				wordRepeats[i]++;
-				if (wordRepeats[i] > 100)
-				{
-					words[i] = "";
-					cutWordsCtn++;
-				}
-				isNewWord = false;
-			}
+        i = 0;
+    check_word_repeats:
+        if (i < wordsCtn)
+        {
+            if (words[i] == word)
+            {
+                wordRepeats[i]++;
+                if (wordRepeats[i] > 100)
+                {
+                    words[i] = "";
+                    cutWordsCtn++;
+                }
+                isNewWord = false;
+            }
 
-			i++;
-			goto check_word_repeats;
-		}
-		if (isNewWord)
-		{
-			words[wordsCtn] = word;
-			wordRepeats[wordsCtn] = 1;
-			wordsCtn++;
-		}
+            i++;
+            goto check_word_repeats;
+        }
+        if (isNewWord)
+        {
+            words[wordsCtn] = word;
+            wordRepeats[wordsCtn] = 1;
+            wordsCtn++;
+        }
 
-		int wordIdx = wordsCtn - 1,
-			pageIdx = wordRepeats[wordIdx] - 1;
-		int pageNum = currString / 45 + 1;
-		wordPages[wordIdx][pageIdx] = pageNum;
+        int wordIdx = wordsCtn - 1,
+            pageIdx = wordRepeats[wordIdx] - 1;
+        int pageNum = currString / 45 + 1;
+        wordPages[wordIdx][pageIdx] = pageNum;
 
-		goto loop_input;
-	}
-	fin.close();
-	cout << "done" << endl;
+        goto loop_input;
+    }
+    fin.close();
+    cout << "done" << endl;
 
-	// TODO: cut off words that enter more than 100 times
+    // TODO: cut off words that enter more than 100 times
 
-	// write to file
-	ofstream fout;
-	fout.open("output.txt");
+    // write to file
+    ofstream fout;
+    fout.open("output.txt");
 
-	i = 0;
+    i = 0;
 loop_output:
-	if (i < wordsCtn)
-	{
-		fout << words[i] << " - ";
+    if (i < wordsCtn)
+    {
+        fout << words[i] << " - ";
 
-		int j = 0;
-		fout << wordPages[i][j];
-		j++;
-	loop_pages:
-		if (wordPages[i][j] != 0)
-		{
-			fout << ", " << wordPages[i][j];
-			j++;
-			goto loop_pages;
-		}
-		fout << endl;
-		i++;
-		goto loop_output;
-	}
-	fout.close();
+        int j = 0;
+        fout << wordPages[i][j];
+        j++;
+    loop_pages:
+        if (wordPages[i][j] != 0)
+        {
+            fout << ", " << wordPages[i][j];
+            j++;
+            goto loop_pages;
+        }
+        fout << endl;
+        i++;
+        goto loop_output;
+    }
+    fout.close();
 
-	// free memory
-	i = 0;
+    // free memory
+    i = 0;
 delete_word_pages:
-	if (i < MaxWords)
-	{
-		delete[] wordPages[i];
-		i++;
-		goto delete_word_pages;
-	}
-	delete[] wordRepeats, words, wordPages;
+    if (i < MaxWords)
+    {
+        delete[] wordPages[i];
+        i++;
+        goto delete_word_pages;
+    }
+    delete[] wordRepeats, words, wordPages;
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
